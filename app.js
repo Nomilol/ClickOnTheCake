54cb384f8562e7173df095343531055ece4e4e6e
Axel <lezinaxel@gmail.com>
2016-10-07 12:06:19 +0200

    Remaniement du code

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
 $(document).ready(function(){ 
-	var cookie = 0;
+	var nbClick = 0;
 	
 	$('.cake').click(function(){
-		cookie ++;
-		$('.counter').text(cookie);
+		nbClick++;
+		$('.counter').text(nbClick);
+		var tab = ["ta vie est nulle","va chercher une petite amie","ton zizi est tout petit","Va jouer à pokemon GO"]
+		var msg = tab[Math.floor(Math.random() * tab.length)]
+		if(nbClick%20 === 0){
+			alert(msg);
+		};
+	});
+});
+		
+
+
+
+
+
+
+		
+
+
+
+
+
 
 		
 			
-
-		var tab = ["ta vie est nulle","va chercher une petite amie","ton zizi est tout petit","Va jouer à pokemon GO"]
-		
-		if(cookie%20 === 0){
-			alert(tab[Math.floor(Math.random() * tab.length)]);
-		};
-		
-
-
-	});
-
-
-});
-
-		
-
-
-
-
-
 
 
