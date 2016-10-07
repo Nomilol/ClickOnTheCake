92f2e55eb76f25b6b655922c7e1ae9dd39d40969
Axel <lezinaxel@gmail.com>
2016-10-07 09:45:07 +0200

    Quatre quart cliker

--- app.js
+++ app.js
@@ -1,5 +1,31 @@
 $(document).ready(function(){
-	//votre code
+	var cookie = 0;
+	
+	$('.cake').click(function(){
+		cookie ++;
+		$('.counter').text(cookie);
+
+		
+			
+
+		var tab = ["ta vie est nulle","va chercher une petite amie","ton zizi est tout petit","Va jouer à pokemon GO"]
+		
+		if(cookie%20 === 0){
+			alert(tab[Math.floor(Math.random() * tab.length)]);
+		};
+		
+
+
+	});
 
 
 });
+
+		
+
+
+
+
+
+
+
